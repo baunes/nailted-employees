bcd0fdef82dc17ce390d804e75790ed9ea73f5b7
Javier Bauset <baunes@protonmail.com>
2020-09-20 09:10:55 +0200

    feat: initialize router

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,10 +6,18 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ContainersModule } from './containers/containers.module';
 import { DefaultLayoutComponent } from './containers/default-layout/default-layout.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule, ContainersModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    ContainersModule,
+    RouterModule.forRoot([{ path: '', component: HomeComponent }]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
